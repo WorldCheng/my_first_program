8e37d3272c08644d988bd76d480d33f949ff9ef0
WorldCheng <1913711216@qq.com>
2025-08-31 14:07:54 +0800

    first commit, version 0.0.1

---
+++ include/hello.h
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace  std;
+
+class HELLO{
+public:
+  void say_hello();
+};
+
+  
---
+++ src/hello.cpp
@@ -0,0 +1,14 @@
+#include"hello.h"
+
+void HELLO::say_hello(){
+  cout<<"Welcome to my program";
+}
+
+int main(){
+
+  HELLO hello;
+
+  hello.say_hello();
+
+  return 0;
+}
